51093346a4f6c0a4a9cb5bc4684e4f5e5f8ef940
TTRademacher <rademacher.tim@gmail.com>
2021-11-19 15:55:36 -0500

    Added new scripts to look at trends in the industrie by region.

---
+++ basicProducerStats.R
@@ -0,0 +1,84 @@
+#========================================================================================
+# Script to explore the producer data by region and see the impact of using all data or 
+# only data with certainty.
+#----------------------------------------------------------------------------------------
+
+# source production data and make sure it was geo-referenced
+#----------------------------------------------------------------------------------------
+if (!exists ('production')) source ('matchClimAndProdData.R') 
+if (!existsFunction ('%>%')) library ('tidyverse')
+
+# colour palette for the regions 
+#----------------------------------------------------------------------------------------
+regionColours <- c ('#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462',
+                    '#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5','#ffed6f','#000000')
+
+# create subset of data with only entries that have no comments (i.e., are certain in geo-referencing) 
+#----------------------------------------------------------------------------------------
+productionSub <- production %>% filter (is.na (comment))
+dim (productionSub) [1]
+dim (production) [1] - dim (productionSub) [1]
+
+# plot number of producers by region over the years
+#----------------------------------------------------------------------------------------
+producersByRegion <- productionSub %>% count (region, year)
+regions <- unique (producersByRegion$region) [which (!is.na (unique (producersByRegion$region)))]
+max (producersByRegion$n)
+par (mfrow = c (2, 1))
+par (mar = c (0, 5, 1, 1))
+plot (NULL, xlim = c (2005, 2021), ylim = c (2300, 3400), xlab = '', ylab = '',
+      axes = FALSE)
+axis (side = 2, las = 1)
+for (r in regions) {
+  tmp <- producersByRegion %>% filter (region == r)
+  lines (x = tmp$year, y = tmp$n, lwd = 2, col = regionColours [which (regions == r)])
+}
+legend (y = 3100, x = 2015, box.lty = 0, 
+        col = regionColours [c (5, 6, 4, 2, 11, 3, 8, 9, 10, 7, 12, 1, 13)], 
+        legend = regions [c (5, 6, 4, 2, 11, 3, 8, 9, 10, 7, 12, 1, 13)], lty = 1, 
+        lwd = 2, bg = 'transparent', cex = 0.6)
+par (mar = c (3, 5, 0, 1))
+plot (NULL, xlim = c (2005, 2021), ylim = c (0, 1100), xlab = '', ylab = '',
+      axes = FALSE)
+axis (side = 1)
+axis (side = 2, las = 1)
+for (r in regions) {
+  tmp <- producersByRegion %>% filter (region == r)
+  lines (x = tmp$year, y = tmp$n, lwd = 2, col = regionColours [which (regions == r)])
+}
+mtext (side = 2, line = 3.5, at = 1200, text = 'Number of producers')
+
+# Plot number of taps by region
+#----------------------------------------------------------------------------------------
+tapsByRegion <- productionSub %>% group_by (region, year) %>% 
+  summarise (nTaps = sum (numberOfTaps), .groups = 'drop')
+par (mfrow = c (2, 1))
+par (mar = c (0, 5, 1, 1))
+plot (NULL, xlim = c (2005, 2021), ylim = c (12, 18), xlab = '', ylab = '',
+      axes = FALSE)
+axis (side = 2, las = 1)
+for (r in regions) {
+  tmp <- tapsByRegion %>% filter (region == r)
+  lines (x = tmp$year, y = tmp$nTaps / 1e6, lwd = 2, col = regionColours [which (regions == r)])
+}
+legend (y = 16, x = 2015, box.lty = 0, 
+        col = regionColours [c (5, 6, 4, 2, 11, 3, 8, 9, 10, 7, 12, 1, 13)], 
+        legend = regions [c (5, 6, 4, 2, 11, 3, 8, 9, 10, 7, 12, 1, 13)], lty = 1, 
+        lwd = 2, bg = 'transparent', cex = 0.4)
+par (mar = c (3, 5, 0, 1))
+plot (NULL, xlim = c (2005, 2021), ylim = c (0, 8.5), xlab = '', ylab = '',
+      axes = FALSE)
+axis (side = 1)
+axis (side = 2, las = 1)
+for (r in regions) {
+  tmp <- tapsByRegion %>% filter (region == r)
+  lines (x = tmp$year, y = tmp$nTaps / 1e6, lwd = 2, col = regionColours [which (regions == r)])
+}
+mtext (side = 2, line = 3.5, at = 9, text = 'Number of taps (millions)')
+
+
+# Mean production by region over time
+#----------------------------------------------------------------------------------------
+
+#========================================================================================
+
--- matchClimAndProdData.R
+++ matchClimAndProdData.R
@@ -13,20 +13,24 @@ if (!exists ('production')) source ('readProductionData.R')
 if (!existsFunction ('dms2char')) library ('sp')
 
 # read file with locations of the municipalities from données Québec
+# downloaded from https://statistique.quebec.ca/pls/hcp/hcp225_fichr_seqnt.hcp225_f1?pvcLangue=fr
+# at the 16th of November 2021
 #----------------------------------------------------------------------------------------
-muniCoord <- readxl::read_excel (path = path.expand ('../data/ISQ_code_geo_20211029.xls'),
-  sheet = 'ISQ_code_geo_20211029',
-  col_names = c ('municipalID',      # unique identifier for municipality
-                 'name',             # name of municipality
-                 'population',       # estimated population of municipality
-                 'cartographicRef',  # cartographic reference 
-                 'latitude',         # latitude of municipality            (DMS)
-                 'longitude',        # longitude of municipality           (DMS)
-                 'landSurface',      # terrestrial surface of municipality (km2)
-                 'totalSurface',     # total surface area of municipality  (km2)
-                 'regionalDivision', # type of regional division
-                 'regionalCode',     # code for regional division
-                 'region'),          # name of the administrative region
+muniCoord <- readxl::read_excel (path = path.expand ('../data/ISQ_code_geo_20211116.xlsx'),
+  sheet = 'ISQ_code_geo_20211116',
+  col_names = c ('municipalID',         # unique identifier for municipality
+                 'municipalStatus',     # status of the municipality (M = Municipality, V = Ville, NO = non-organised territory, CT = Canton, R = Reserve indienne, VL = village, P = Paroisse, ) 
+                 'name',                # name of municipality
+                 'population',          # estimated population of municipality
+                 'dateOfIncorporation', # date of incorporation of the municipality
+                 'cartographicRef',     # cartographic reference 
+                 'latitude',            # latitude of municipality            (DMS)
+                 'longitude',           # longitude of municipality           (DMS)
+                 'landSurface',         # terrestrial surface of municipality (km2)
+                 'totalSurface',        # total surface area of municipality  (km2)
+                 'regionalDivision',    # type of regional division
+                 'regionalCode',        # code for regional division
+                 'region'),             # name of the administrative region
   skip = 1)
 
 # decompose the latitudes and longitudes in degrees, minutes, and seconds
