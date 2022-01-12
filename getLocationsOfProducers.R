85e99e2bc76c4fa24f90764e97bcc9370376c5e7
TTRademacher <rademacher.tim@gmail.com>
2022-01-12 14:25:14 -0500

    Check and update producer locations.

--- getLocationsOfProducers.R
+++ getLocationsOfProducers.R
@@ -9,7 +9,10 @@ if (!existsFunction ('dms2char')) library ('sp')
 
 # list of unique municipalities with producers
 #----------------------------------------------------------------------------------------
-listOfMunicipalities <- unique (production$municipality) 
+listOfMunicipalities <- production %>% 
+  select (municipality, region) %>% 
+  unique () %>% 
+  filter (!is.na (municipality) & !is.na (region))
 
 # get list of municipalities from the government of Quebec
 #----------------------------------------------------------------------------------------
@@ -45,11 +48,51 @@ muniCoord$lon <- -as.numeric (char2dms (from = muniCoord$longitude,
 
 # Select only municipalities with producers
 #----------------------------------------------------------------------------------------
-producerLocations <- muniCoord %>% filter (name %in% listOfMunicipalities)
+producerLocations <- tibble (municipalID = NA, municipalStatus = NA, name = NA, 
+                             population = NA, dateOfIncorporation = NA, 
+                             cartographicRef = NA, latitude = NA, longitude = NA, 
+                             landSurface = NA, totalSurface = NA, regionalDivision = NA, 
+                             regionalCode = NA, region = NA, lat = NA, lon = NA)
+for (r in 1: dim (listOfMunicipalities) [1]) {
+  i <- which (listOfMunicipalities$municipality [r] == muniCoord$name &
+              listOfMunicipalities$region [r] == muniCoord$region)
+  
+  if (length (i) >= 2) {
+    print (paste (i, listOfMunicipalities$municipality [r], 
+                  listOfMunicipalities$region [r], muniCoord$municipalStatus [i]))
+    if ('R' %in% muniCoord$municipalStatus [i]) {
+      i <- which (listOfMunicipalities$municipality [r] == muniCoord$name &
+                    listOfMunicipalities$region [r] == muniCoord$region &
+                    muniCoord$municipalStatus != "R")
+    } else if ('P' %in% muniCoord$municipalStatus [i]) {
+      i <- which (listOfMunicipalities$municipality [r] == muniCoord$name &
+                    listOfMunicipalities$region [r] == muniCoord$region &
+                    muniCoord$municipalStatus == "P")
+    } else if ('VL' %in% muniCoord$municipalStatus [i]) {
+      i <- which (listOfMunicipalities$municipality [r] == muniCoord$name &
+                    listOfMunicipalities$region [r] == muniCoord$region &
+                    muniCoord$municipalStatus != "VL")
+    } else if ('CT' %in% muniCoord$municipalStatus [i]) {
+      i <- which (listOfMunicipalities$municipality [r] == muniCoord$name &
+                    listOfMunicipalities$region [r] == muniCoord$region &
+                    muniCoord$municipalStatus == "CT")
+    }
+  }
+  if (length (i) == 1) {
+    producerLocations <- producerLocations %>% add_row (muniCoord [i, ])  
+  } else if (length (i) == 2) {
+    stop ("There are two municipalities!")
+    producerLocations <- producerLocations %>% add_row (muniCoord [i, ])
+  } else {
+    stop ("Stop!")
+  }
+}
+# delete empty first row to fall back to 643 locations
+producerLocations <- producerLocations [-1, ]
 
 # write file with locations for BioSIM
 #----------------------------------------------------------------------------------------
-producerLocations <- producerLocations %>% select (name, lat, lon) %>% 
+producerLocations <- producerLocations %>% select (name, lat, lon, region) %>% 
   mutate (KeyID = 1:dim (producerLocations)[1]) %>% relocate (KeyID) %>%
   rename (Name = name, Latitude = lat, Longitude = lon)
 write_csv (producerLocations, "../data/producerLocations.csv")
